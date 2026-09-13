7118fdf77835724f056e122da972ffa567e5fdf8
Sam <sam@frysteen.com.au>
2026-09-13 23:27:04 +1000

    Version 0.1.7

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: bricks, acf
 Requires at least: 6.0
 Tested up to: 7.1
 Requires PHP: 7.4
-Stable tag: 0.1.6
+Stable tag: 0.1.7
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -32,6 +32,9 @@ Extras:
 * Default To WP Editor: open the WordPress editor on posts with no Bricks content.
 
 == Changelog ==
+= 0.1.7 =
+* Exported settings file name reads properly for the site it came from.
+
 = 0.1.6 =
 * Updates page can now export the settings to a JSON file and import them on another site.
 
--- socialbump-bricks-tweaks.php
+++ socialbump-bricks-tweaks.php
@@ -3,7 +3,7 @@
  * Plugin Name: SocialBUMP Bricks Tweaks
  * Plugin URI:  https://socialbump.com.au
  * Description: A home for SocialBUMP custom Bricks Builder elements and site tweaks. Turn each one on or off under SB Bricks Tweaks.
- * Version:     0.1.6
+ * Version:     0.1.7
  * Requires at least: 6.0
  * Requires PHP: 7.4
  * Author:      SocialBUMP
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'SBBT_VERSION', '0.1.6' );
+define( 'SBBT_VERSION', '0.1.7' );
 define( 'SBBT_FILE', __FILE__ );
 define( 'SBBT_PATH', plugin_dir_path( __FILE__ ) );
 define( 'SBBT_URL', plugin_dir_url( __FILE__ ) );
